f1763776f8010c9f174657e52eb28c273633c8e2
Charlie-007-star <sujinsr2457@gmail.com>
2022-01-31 22:56:52 +0530

    react typescript

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+let hello;
+hello = 'welcome to react typescript';
+console.log(hello);
---
+++ index.ts
@@ -0,0 +1,4 @@
+let hello: string;
+hello = 'welcome to react typescript';
+
+console.log(hello);
